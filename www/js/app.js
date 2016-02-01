50d1d7e64ec7138d564a208b0758c29053630b5b
fiona <fiona@shrek>
2016-02-01 15:49:26 +0200

    Added ngCordova and inject $cordovaSplashscreen and $timeout.

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,28 @@
+/* global StatusBar */
+
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var app = angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.services']);
+var app = angular.module('conFusion', ['ionic', 'ngCordova', 'conFusion.controllers', 'conFusion.services']);
 
-app.run(function ($ionicPlatform, $rootScope, $ionicLoading) {
+app.run(function ($ionicPlatform, $rootScope, $ionicLoading, $cordovaSplashscreen, $timeout) {
     $ionicPlatform.ready(function () {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             cordova.plugins.Keyboard.disableScroll(true);
-
         }
         if (window.StatusBar) {
             // org.apache.cordova.statusbar required
             StatusBar.styleDefault();
         }
+        $timeout(function () {
+            $cordovaSplashscreen.hide();
+        }, 2000);
     });
     $rootScope.$on('loading:show', function () {
         $ionicLoading.show({
